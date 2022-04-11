de4ddf88945e14ab5bd7c3e513e80cac871018a1
Moni1311 <96513810+Moni1311@users.noreply.github.com>
2022-04-11 14:56:35 +0530

    final project

---
+++ AngularApplication/EmployeeManagementSystem/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AchieveComponent } from './achieve/achieve.component';
+
+
+const routes: Routes = [{path: 'employees', component: EmployeeListComponent},
+{path: 'create-employee', component: CreateEmployeeComponent},
+{path: '', redirectTo: 'employees', pathMatch: 'full'},
+{path: 'update-employee/:id', component: UpdateEmployeeComponent},
+{path: 'employee-details/:id', component: EmployeeDetailsComponent},
+{path: 'about', component: AboutComponent},
+{path: 'contact', component: ContactComponent},
+{path: 'achieve', component: AchieveComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ AngularApplication/EmployeeManagementSystem/src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    id: number;
+    firstName: string ;
+    lastName: string ;
+    emailId: string;
+    designation:string;
+    salary:number;
+    contact:number;
+    address: string;
+}
